3c25ba24b75cbdf1ff3e8e77795c440eafa9bc60
solider <dahaoren502@gmail.com>
2019-09-24 21:20:03 +0800

    并发逻辑

--- src/server.go
+++ src/server.go
@@ -1,11 +1,42 @@
 /*
- */
+1.尝试测试工具
+2.git工具尝试
+3.文档查看工具使用
+*/
 package ldlnet
 
 import (
+	"bufio"
 	"fmt"
+	"net"
 )
 
 func Serve() {
-	fmt.Printf("")
+	lister, err := net.Listen("tcp", "127.0.0.1:8888")
+	if err != nil {
+		return
+	}
+
+	fmt.Println("服务启动成功，监听端口8888")
+	defer lister.Close()
+
+	for {
+		conn, err := lister.Accept()
+		if err != nil {
+			return
+		}
+
+		fmt.Println("用户接入")
+
+		go handleConn(conn)
+	}
+}
+
+func handleConn(conn net.Conn) {
+	fmt.Printf("nihaoma")
+	input := bufio.NewScanner(conn)
+	for input.Scan() {
+		fmt.Printf("%s", input.Text())
+	}
+	conn.Close()
 }
